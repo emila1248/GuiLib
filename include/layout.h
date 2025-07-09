b704c18311a02b32b494975d934732009f41fa5c
EL <emila1248@gmail.com>
2025-07-08 21:15:54 -0400

    Updated

--- include/layout.h
+++ include/layout.h
@@ -1,9 +1,29 @@
 #pragma once
 #include "raylib.h"
 #include "element.h"
+
 #include <vector>
+#include <memory>
+
+/****** Base class that all "Layout" elements inherit from ******/
 
 class Layout : public Element
 {
+private:
+    std::vector<std::shared_ptr<Element>> elements;
 
+public:
+    void AddElement(std::shared_ptr<Element> element) {
+        elements.push_back(element);
+    }
+
+    void Draw() override {
+        for (auto& element : elements)
+            element->Draw();
+    }
+
+    void Update() override {
+        for (auto& element : elements)
+            element->Update();
+    }
 };
